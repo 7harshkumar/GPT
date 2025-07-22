18cc0239703b63a85aa815ffcd7fc16597b06c02
Harsh Kumar <221220025@nitdelhi.ac.in>
2025-07-22 11:23:58 +0530

    Add

--- Backend/server.js
+++ Backend/server.js
@@ -5,26 +5,30 @@ import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-
 app.use("/api", chatRoutes);
 
-app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
-    connectDB();
-});
-
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("Connected with Database!");
-    } catch(err) {
-        console.log("Failed to connect with Db", err);
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.log("Failed to connect with DB", err);
+        process.exit(1);
     }
-}
+};
+
+connectDB();
+
 
 
 // app.post("/test", async (req, res) => {
